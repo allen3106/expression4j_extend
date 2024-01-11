8f8875102032252577816d3c98dc86022d5a13de
allen3106 <allen.gege@qq.com>
2024-01-11 11:05:29 +0800

    first commit

--- src/com/zlus/expession4j/extend/constant/MathematicalElementType.java
+++ src/com/zlus/expession4j/extend/constant/MathematicalElementType.java
@@ -3,8 +3,8 @@ package com.zlus.expession4j.extend.constant;
 public interface MathematicalElementType {
     int DOUBLE=1;
     int COMPLEX=2;
-    int INTEGER=7;
-    int BOOLEAN = 6;
     int STRING = 5;
+    int BOOLEAN = 6;
+    int INTEGER=7;
     int NULL = 8;
 }
